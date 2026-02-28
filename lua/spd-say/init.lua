25e6009e5e2034c2d2c4ee4f9631bd64b203e242
Corey B <14075562+coreyb-git@users.noreply.github.com>
2026-02-28 22:32:50 +1000

    Fix: Wasn't updating the opts table correctly

--- lua/spd-say/init.lua
+++ lua/spd-say/init.lua
@@ -6,7 +6,9 @@ local opts = require("spd-say.opts")
 require("spd-say.cmds").setup()
 
 M.setup = function(custom_opts)
-	vim.tbl_extend("force", opts, custom_opts)
+	for k, v in pairs(custom_opts) do
+		opts[k] = v
+	end
 end
 
 return M
